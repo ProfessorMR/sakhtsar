ce847663e9ef026fe6da38660ea73113ecc45c95
professor_mr <professormr29@gmail.com>
2022-03-24 13:53:54 +0430

    rework submit content index.html and css and js

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -6,14 +6,16 @@ const chooseFileSubmitContent = document.getElementById('choose-file-submit-cont
 const btnSubmitContent = document.getElementById('btn-submit-content');
 const showCategories = document.querySelector(".showCategories");
 const showCategoriesName = document.querySelectorAll('.itemCategory');
+const uploadSubmitConntetLabel = document.querySelector('.upload-submit-content').children[1];
 let validate_1 = false;
 let validate_2 = false;
 let validate_3 = false;
 let validate_4 = false;
 
-
 fullNameSubmitContent.oninput = () => {
+
     const fullNameSubmitContentValue = fullNameSubmitContent.value.trim();
+
     if(fullNameSubmitContentValue != ''){
         validate_1 = true;
     }
@@ -33,9 +35,9 @@ fullNameSubmitContent.oninput = () => {
 subjectSubmitContent.oninput = () => {
 
     const subjectSubmitContentValue = subjectSubmitContent.value.trim();
+
     if(subjectSubmitContentValue != ''){
         validate_2 = true;
-        
     }
     else{
         validate_2= false;
@@ -50,16 +52,16 @@ subjectSubmitContent.oninput = () => {
     }
 }
 
-
 categorysSubmitContent.onclick = () => {
     showCategories.classList.toggle("activeShowCategory");
-    showCategoriesName.forEach(i => i.addEventListener('click',function(){
+}
 
-        categorysSubmitContent.value = i.innerHTML;
-        console.log(categorysSubmitContent);
-        showCategories.classList.remove("activeShowCategory");
-        validate_3 = true;
-    }));
+showCategoriesName.forEach(i => i.addEventListener('click',function(){
+
+    categorysSubmitContent.value = i.innerHTML;
+    showCategories.classList.remove("activeShowCategory");
+    validate_3 = true;
+
     if(validate_1 && validate_2 && validate_3 && validate_4){
         btnSubmitContent.disabled = false;
         btnSubmitContent.classList.add('activeBtnSubmitContent');
@@ -68,18 +70,15 @@ categorysSubmitContent.onclick = () => {
         btnSubmitContent.disabled = true;
         btnSubmitContent.classList.remove('activeBtnSubmitContent');
     }
-}
+}));
 
-chooseFileSubmitContent.onclick = () => {
-    const getTypeOld = chooseFileSubmitContent.getAttribute('type');
-    if(getTypeOld == 'text'){
-    chooseFileSubmitContent.setAttribute('type', 'file');
-        chooseFileSubmitContent.oninput = () =>{
-        chooseFileSubmitContent.value.split("\\").pop();
-        
-        }
-    }
+chooseFileSubmitContent.oninput = () =>{
+    let uploadName = chooseFileSubmitContent.value.split("\\").pop()
+
+    uploadSubmitConntetLabel.innerHTML = uploadName;
+    uploadSubmitConntetLabel.parentElement.classList.add('activeColorToUploader');
     validate_4 = true;
+
     if(validate_1 && validate_2 && validate_3 && validate_4){
         btnSubmitContent.disabled = false;
         btnSubmitContent.classList.add('activeBtnSubmitContent');
@@ -89,6 +88,5 @@ chooseFileSubmitContent.onclick = () => {
         btnSubmitContent.classList.remove('activeBtnSubmitContent');
     }
 }
-
 
 //{end} Submit content
